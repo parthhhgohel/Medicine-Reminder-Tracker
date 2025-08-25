4e72e4cfb8ab89fe3a93baa4a9735940910fc3af
parthhhgohel <parthgohel806@gmail.com>
2025-08-25 19:38:29 +0530

    form and lists completed

---
+++ script.js
@@ -0,0 +1,12 @@
+// const dropdown = document.querySelector('.dropdown');
+// const profileDropdown = document.querySelector('.profile-dropdown');
+
+// // Show dropdown on mouseenter
+// profileDropdown.addEventListener('mouseenter', () => {
+//     dropdown.classList.add('active');
+// });
+
+// // Hide dropdown on mouseleave
+// profileDropdown.addEventListener('mouseleave', () => {
+//     dropdown.classList.remove('active');
+// });
